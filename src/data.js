435643fe9725b3eede625aad589e120978f65ecf
jetix2 <yourname@example.com>
2021-06-25 01:03:08 +0300

    Continuious Design

---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const projects = [
+  {
+    title: "NettoDino",
+    subtitle: "MERN Stack",
+    description:
+      "Simple and fast ECommerce application. the users can register, sign in into their accounts, purchase from diffrent sellers using PayPal's 3rd API Calls. ",
+    image: "./project-1.gif",
+    link: "https://nettodino.netlify.app",
+  },
+  {
+    title: "BobBeat Task App",
+    subtitle: "React Hooks",
+    description:
+      "Bobbeat - a task that does not stink!. the user enters a HTML Page filled with color and information about the app and gets to test the DEMO app with REACT Hooks.",
+    image: "./project-2.gif",
+    link: "https://bobbeat.netlify.app/",
+  },
+  {
+    title: "WeatherApp",
+    subtitle: "React + Redux",
+    description:
+      "Responsive Weather App in REACT And Redux. The User can add his location and get realtime weather conditions using Accuweathers 3rd Party API Calls.",
+    image: "./project-3.gif",
+    link: "https://orelweather.netlify.app/",
+  },
+  {
+    title: "Catch Me Game",
+    subtitle: "Vanila JavaScript",
+    description:
+      "a simple yet exciting game of catch using the mouse to press on the catch me bar. the game operates using pure Vanila JavaScript and saving the scores to the local storage of the user.",
+    image: "./project-4.gif",
+    link: "https://smallgameproject.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      '"If you get up in the morning and think the future is going to be better,it is a bright day. Otherwise, its not. "',
+    image: "downloadd.jpg",
+    name: "Elon Musk",
+    company: "SpaceX",
+  },
+  {
+    quote:
+    '"Leadership means that a group, large or small, is willing to entrust authority to a person who has shown judgment, wisdom, personal appeal, and proven competence."',
+    image: "./download.jpg",
+    name: "Walt Disney",
+    company: "Disney",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React + React Hooks",
+  "Redux",
+  "Node.JS",
+  "MongoDB",
+  "Bootstrap/Semantic Ui/Tailwind Css",
+  "REST API",
+  "MERN Stack Deployment"
+];
